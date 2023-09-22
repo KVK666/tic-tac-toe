771688f9f74d8c96743594ce47fc59f3e9bb83df
KVK666 <vamsik823@gmail.com>
2023-09-22 06:07:01 +0530

    fixed the major issue

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -4,20 +4,23 @@ import "./MainComponent.css";
 export default function MainComponent() {
   const [boxes, setBoxes] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [previousPlayer, setPreviousPlayer] = useState(null);
 
   function handleClick(boxIndex) {
-    // Check if the box is already filled or game is already won
+    // Check if the box is already filled or the game is already won
     if (boxes[boxIndex] === null && !Win()) {
       const updatedBoxes = [...boxes];
       updatedBoxes[boxIndex] = currentPlayer;
 
       setBoxes(updatedBoxes);
+      setPreviousPlayer(currentPlayer);
+
+      // Toggle the player's turn
       setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
     }
   }
 
-
-function Win() {
+  function Win() {
     if (
       checkCondition(boxes[0], boxes[1], boxes[2]) ||
       checkCondition(boxes[3], boxes[4], boxes[5]) ||
@@ -30,14 +33,15 @@ function Win() {
     ) {
       return true;
     }
-    return false;}
+    return false;
+  }
 
-    function checkCondition(a, b, c) {
-        if ( a && a === b && a === c) {
-          return true;
-        }
-        return false;
-      }
+  function checkCondition(a, b, c) {
+    if (a && a === b && a === c) {
+      return true;
+    }
+    return false;
+  }
 
   function Draw() {
     return boxes.every((box) => box !== null) && !Win();
@@ -45,11 +49,11 @@ function Win() {
 
   let message = "";
   if (Win()) {
-    message = "Winner: " + currentPlayer;
+    message = "Winner: " + previousPlayer;
   } else if (Draw()) {
     message = "Draw";
   } else {
-    message = "Next player: " + (currentPlayer === "X" ? "O" : "X");
+    message = "Next player: " + currentPlayer;
   }
 
   return (
